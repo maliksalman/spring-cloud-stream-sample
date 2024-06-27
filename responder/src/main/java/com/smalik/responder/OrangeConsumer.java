9bf982bbb6baf117be48afad1bb0f1c70f271a86
Salman Malik <salman@the-tech-warriors.com>
2024-06-27 19:58:24 -0400

    Adding orange-consumer

---
+++ responder/src/main/java/com/smalik/responder/OrangeConsumer.java
@@ -0,0 +1,12 @@
+package com.smalik.responder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrangeConsumer {
+
+    private Logger log = LoggerFactory.getLogger(OrangeConsumer.class);
+
+}
