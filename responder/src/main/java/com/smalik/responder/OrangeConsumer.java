5e8c353f7b741db7d8dc8d32b5c72a44d1c3d5a6
Salman Malik <salman@the-tech-warriors.com>
2024-06-27 20:04:05 -0400

    Adding orange-consumer

--- responder/src/main/java/com/smalik/responder/OrangeConsumer.java
+++ responder/src/main/java/com/smalik/responder/OrangeConsumer.java
@@ -2,7 +2,12 @@ package com.smalik.responder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 @Service
 public class OrangeConsumer {
