1bf40349cc666572a1ec44c2aa2f3bcaa9950343
Salman Malik <salman@the-tech-warriors.com>
2024-06-28 13:32:35 -0400

    Adding shell of a PieConsumer

---
+++ initiator/src/main/java/com/smalik/initiator/PieConsumer.java
@@ -0,0 +1,18 @@
+package com.smalik.initiator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+@Service
+public class PieConsumer {
+
+    private Logger log = LoggerFactory.getLogger(PieConsumer.class);
+
+    // pie event definition
+
+    // bean to handle receiving the pie event
+}
